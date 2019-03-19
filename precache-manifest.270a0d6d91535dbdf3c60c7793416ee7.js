3b4738a44167de74fa4869a189bf60052554815d
yiyuan <iwillbegood@live.com>
2019-03-19 13:07:06 +0800

    latest production build

---
+++ precache-manifest.270a0d6d91535dbdf3c60c7793416ee7.js
@@ -0,0 +1,418 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a864e0e02a55c4781a6511be5649d72",
+    "url": "/static/media/DB20020010.7a864e0e.pdf"
+  },
+  {
+    "revision": "5012489cb9824ec0a499",
+    "url": "/static/css/main.f64afba3.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "546c7201e71bea55c843",
+    "url": "/static/js/2.546c7201.chunk.js"
+  },
+  {
+    "revision": "a1d40c3876bd1460ff1e9e3858d699c7",
+    "url": "/static/media/user_profile.a1d40c38.png"
+  },
+  {
+    "revision": "d0406ecc8b94d0b8405f5899a158221f",
+    "url": "/static/media/honor-bg.d0406ecc.jpg"
+  },
+  {
+    "revision": "47d961196c7e652833d388c1ba78a183",
+    "url": "/static/media/credit_02.47d96119.jpg"
+  },
+  {
+    "revision": "894d61a41442f4a39fffcf86ffe480ea",
+    "url": "/static/media/img-paper.894d61a4.jpg"
+  },
+  {
+    "revision": "df622dcdbdc6586ea9ee7a4d2e796dba",
+    "url": "/static/media/img-thesis.df622dcd.jpg"
+  },
+  {
+    "revision": "bf46290a2c7cefc59de7331f259cb448",
+    "url": "/static/media/home_bg.bf46290a.jpg"
+  },
+  {
+    "revision": "040be994534dd6c59e4768b4fa2431dc",
+    "url": "/static/media/YaHuiChang.040be994.jpg"
+  },
+  {
+    "revision": "06d062103f7697438bf1163f11cc099f",
+    "url": "/static/media/people_bg.06d06210.jpg"
+  },
+  {
+    "revision": "0758e078069e06651bcadd93ba60bd7d",
+    "url": "/static/media/research-bg.0758e078.jpg"
+  },
+  {
+    "revision": "b2b4a7119461d4b4bc68a9439d6baafe",
+    "url": "/static/media/link_bg.b2b4a711.jpg"
+  },
+  {
+    "revision": "b162ca5aa66e425ef3157988ba49a206",
+    "url": "/static/media/member01.b162ca5a.jpg"
+  },
+  {
+    "revision": "f0f22938ae0231bb0cb8e4901d2fc61b",
+    "url": "/static/media/member02.f0f22938.jpg"
+  },
+  {
+    "revision": "834d4fe1532f9ba8ee75e1ae69bf03ee",
+    "url": "/static/media/member03.834d4fe1.jpg"
+  },
+  {
+    "revision": "7a5e61da44a3d83477b25d120bcba096",
+    "url": "/static/media/member04.7a5e61da.jpg"
+  },
+  {
+    "revision": "19845a12c072dab3e26761c62c6b1625",
+    "url": "/static/media/keyboy.19845a12.jpg"
+  },
+  {
+    "revision": "15b7f6768c9080e2c9bc861fa553ce58",
+    "url": "/static/media/misugs.15b7f676.jpeg"
+  },
+  {
+    "revision": "cfadc1c6336faee3150355e6b3e38907",
+    "url": "/static/media/weixi.cfadc1c6.jpeg"
+  },
+  {
+    "revision": "77c3b67900b8549328522a6159810fb0",
+    "url": "/static/media/weiqi.77c3b679.jpg"
+  },
+  {
+    "revision": "4dfbb234f1426e50bfe031319e3c9829",
+    "url": "/static/media/yiyuan.4dfbb234.jpg"
+  },
+  {
+    "revision": "54afc1f296d9be72fbc753b367c8cc6d",
+    "url": "/static/media/hanbao.54afc1f2.jpeg"
+  },
+  {
+    "revision": "50e4f73736e5c01f685a60d441772d3b",
+    "url": "/static/media/leaf.50e4f737.jpeg"
+  },
+  {
+    "revision": "d153031b8d76077a86b3af46975255fd",
+    "url": "/static/media/vicky.d153031b.jpg"
+  },
+  {
+    "revision": "182bf1a6274b9d6d1f1fb34044f499c0",
+    "url": "/static/media/huangji.182bf1a6.jpg"
+  },
+  {
+    "revision": "d6f4da1ab5115ec5dc857631dcf1ae00",
+    "url": "/static/media/heyu.d6f4da1a.jpg"
+  },
+  {
+    "revision": "4f188d26c7e2ab556684301c033e5b31",
+    "url": "/static/media/taifeng.4f188d26.jpg"
+  },
+  {
+    "revision": "3d6d3bd5656cb4824820accd72b242c9",
+    "url": "/static/media/shangkai.3d6d3bd5.jpg"
+  },
+  {
+    "revision": "56f21993d48bc28d6ad8893ecbd7176f",
+    "url": "/static/media/zhiwei.56f21993.jpg"
+  },
+  {
+    "revision": "299a993b198994349ed5a2d26d30a870",
+    "url": "/static/media/credit_01.299a993b.jpg"
+  },
+  {
+    "revision": "d30c662e56a2a673aae048b5047cf730",
+    "url": "/static/media/credit_04.d30c662e.jpg"
+  },
+  {
+    "revision": "934cfbf7f4b781aca838850bdfe6ac34",
+    "url": "/static/media/credit_05.934cfbf7.jpg"
+  },
+  {
+    "revision": "9003f28ccd56f8aedd4123196cac2f76",
+    "url": "/static/media/credit_06.9003f28c.jpg"
+  },
+  {
+    "revision": "52c1d1bdb280c8479f1e3b099577f37a",
+    "url": "/static/media/default.52c1d1bd.jpg"
+  },
+  {
+    "revision": "f80b3c12a8bcfd6d5becbb71a15f9014",
+    "url": "/static/media/DB20150533.f80b3c12.pdf"
+  },
+  {
+    "revision": "f80b3c12a8bcfd6d5becbb71a15f9014",
+    "url": "/static/media/DB20150535.f80b3c12.pdf"
+  },
+  {
+    "revision": "fecd6dbe43fb3e2386a4bdfffa30b697",
+    "url": "/static/media/DB20150537.fecd6dbe.pdf"
+  },
+  {
+    "revision": "23ef6edc577d7fc9b6cd511ead0fb88e",
+    "url": "/static/media/DB20010008.23ef6edc.pdf"
+  },
+  {
+    "revision": "79229c54534c7ce14650a0c169380426",
+    "url": "/static/media/DB20010009.79229c54.pdf"
+  },
+  {
+    "revision": "abd71cdb29715ea4773b16b8948ffb58",
+    "url": "/static/media/DB20020011.abd71cdb.pdf"
+  },
+  {
+    "revision": "1788d1086aac9c346b1c77bc6b7bbf4c",
+    "url": "/static/media/DB20030013.1788d108.pdf"
+  },
+  {
+    "revision": "93f6631079a833b8f7de533f70ffbcd7",
+    "url": "/static/media/DB20070401.93f66310.pdf"
+  },
+  {
+    "revision": "fd55d530602c10da067a172890315a71",
+    "url": "/static/media/DB20100471.fd55d530.pdf"
+  },
+  {
+    "revision": "9306edf9c6835abe1c3bb47208b05392",
+    "url": "/static/media/DBPT20100471.9306edf9.ppt"
+  },
+  {
+    "revision": "e9311891b121f7a74f355363c43f0406",
+    "url": "/static/media/DB20100473.e9311891.pdf"
+  },
+  {
+    "revision": "336c8f8d06592c5bb595f39f2b21e2eb",
+    "url": "/static/media/DBPT20100473.336c8f8d.ppt"
+  },
+  {
+    "revision": "9b04ea71262c250c4e6be00d90e243d7",
+    "url": "/static/media/DB20110490.9b04ea71.pdf"
+  },
+  {
+    "revision": "cc044633911b5e5a3d9dc2da0f54e0e1",
+    "url": "/static/media/DBPT20110490.cc044633.ppt"
+  },
+  {
+    "revision": "3b87b50852c4f54be6f7cad6189cc8b1",
+    "url": "/static/media/DB20130508.3b87b508.pdf"
+  },
+  {
+    "revision": "83577ad4a372bcc316452fa2b15a7ae5",
+    "url": "/static/media/DBPT20130508.83577ad4.ppt"
+  },
+  {
+    "revision": "5012489cb9824ec0a499",
+    "url": "/static/js/main.5012489c.chunk.js"
+  },
+  {
+    "revision": "c5d5ef378a06ef27b19b45966f826f4c",
+    "url": "/static/media/DB20030030.c5d5ef37.doc"
+  },
+  {
+    "revision": "fc51e17dfb6a65bc9170cb6da802329d",
+    "url": "/static/media/DBPT20030030.fc51e17d.ppt"
+  },
+  {
+    "revision": "1f604568f1e45af4647931397db04c44",
+    "url": "/static/media/DB20030031.1f604568.doc"
+  },
+  {
+    "revision": "f2048749a2adb2239b4f3cf0e2f1fcba",
+    "url": "/static/media/DBPT20030031.f2048749.ppt"
+  },
+  {
+    "revision": "375b6e5cac67889622d74147bd98546b",
+    "url": "/static/media/DB04.70098.375b6e5c.pdf"
+  },
+  {
+    "revision": "5c6fdd287d2071b848ced7d645f436a0",
+    "url": "/static/media/DBPT04.70098.5c6fdd28.ppt"
+  },
+  {
+    "revision": "456d989ae8aeec098dca81c73e7355a4",
+    "url": "/static/media/DB20060164.456d989a.doc"
+  },
+  {
+    "revision": "58d3ac82bb6d7dfc7d77e78cd68e9533",
+    "url": "/static/media/DBPT20060164.58d3ac82.ppt"
+  },
+  {
+    "revision": "bf1d0286ff3e46fe87ec64f01d3466f9",
+    "url": "/static/media/DB20060254.bf1d0286.pdf"
+  },
+  {
+    "revision": "7c037f28d00a8827aee0a6ff51e607ca",
+    "url": "/static/media/DBPT20060254.7c037f28.ppt"
+  },
+  {
+    "revision": "ac9e88958e1ada1c4ff5ca5db3cdc951",
+    "url": "/static/media/DB06年0255.ac9e8895.doc"
+  },
+  {
+    "revision": "88c55db604692cccbf4925156968a3b5",
+    "url": "/static/media/DBPT06年0255.88c55db6.ppt"
+  },
+  {
+    "revision": "35dace8c50e72340f215970a641e182f",
+    "url": "/static/media/DB20090453.35dace8c.doc"
+  },
+  {
+    "revision": "a3d09512cafcef55b7bf9d9153989e06",
+    "url": "/static/media/DBPT20090453.a3d09512.ppt"
+  },
+  {
+    "revision": "25f1628e98fd907ee27c5a6a49764dd4",
+    "url": "/static/media/DB20090457.25f1628e.pdf"
+  },
+  {
+    "revision": "b922392805c6ea04fb14be0a12fa7ef6",
+    "url": "/static/media/DBPT20090457.b9223928.ppt"
+  },
+  {
+    "revision": "5a4feee5d6fb0f00ec76438c10baeb0b",
+    "url": "/static/media/DB20090458.5a4feee5.doc"
+  },
+  {
+    "revision": "4b4395d4d919ca7c3e68b21e57cdf13e",
+    "url": "/static/media/DBPT20090458.4b4395d4.ppt"
+  },
+  {
+    "revision": "a03aa144c159ca004444eb6ad5ff0c75",
+    "url": "/static/media/DB20100459.a03aa144.pdf"
+  },
+  {
+    "revision": "bcc29490d5b48bec8af38d760a3be538",
+    "url": "/static/media/DBPTPT200459.bcc29490.ppt"
+  },
+  {
+    "revision": "7a31da1edb2a6d99d3b88ae8349f671e",
+    "url": "/static/media/DB20100460.7a31da1e.pdf"
+  },
+  {
+    "revision": "640fe1908771be70d9d4bdbca4b87dc7",
+    "url": "/static/media/DBPT20100460.640fe190.ppt"
+  },
+  {
+    "revision": "31a0d9c2d528d2bba6831f9b198a18c6",
+    "url": "/static/media/DB20110462.31a0d9c2.pdf"
+  },
+  {
+    "revision": "c3749af018be7947606b4480220a8e99",
+    "url": "/static/media/DBPT20110462.c3749af0.ppt"
+  },
+  {
+    "revision": "98d0554487ced935646511ffb238c7fa",
+    "url": "/static/media/DB20110463.98d05544.pdf"
+  },
+  {
+    "revision": "e9870513a64dc492723aadbccda30a10",
+    "url": "/static/media/DBPT20110463.e9870513.ppt"
+  },
+  {
+    "revision": "27b1b95144c77e664799fffa7c1d7509",
+    "url": "/static/media/DB20130465.27b1b951.pdf"
+  },
+  {
+    "revision": "3982c8452020b87212acce69943c1a7c",
+    "url": "/static/media/DB20130466.3982c845.pdf"
+  },
+  {
+    "revision": "8d47515ac2a1611f7d2dfc146ec9ab80",
+    "url": "/static/media/DBPT20130466.8d47515a.ppt"
+  },
+  {
+    "revision": "4d86fb606ab37e9f947151d25c520b91",
+    "url": "/static/media/DB20140467.4d86fb60.pdf"
+  },
+  {
+    "revision": "280aeca72ac7ba126a122f9c2f2ac550",
+    "url": "/static/media/DBPT20140467.280aeca7.ppt"
+  },
+  {
+    "revision": "33959613d645857a8e5ab9ae57e1b11a",
+    "url": "/static/media/DB20140468.33959613.pdf"
+  },
+  {
+    "revision": "712586e4f55e43bc479d2aba885d4a4b",
+    "url": "/static/media/DBPT20140468.712586e4.ppt"
+  },
+  {
+    "revision": "2c9451c2a350a91211aebba63e260d5f",
+    "url": "/static/media/DB20150268.2c9451c2.pdf"
+  },
+  {
+    "revision": "832a30bac5d7beac7e3c4ac13faa69cb",
+    "url": "/static/media/DB20150268.832a30ba.pptx"
+  },
+  {
+    "revision": "0c9dd9ad34eddbb2231508a1472d9f63",
+    "url": "/static/media/DB20150475.0c9dd9ad.pdf"
+  },
+  {
+    "revision": "00fdc1c9a1b657f68df96bac241ef102",
+    "url": "/static/media/DBPTPT200475.00fdc1c9.ppt"
+  },
+  {
+    "revision": "270422cc0a1a8d24242fe72f73bb899e",
+    "url": "/static/media/DB20160476.270422cc.pdf"
+  },
+  {
+    "revision": "892d668236084d244eb8791c95f8de1a",
+    "url": "/static/media/DBPTPT200476.892d6682.ppt"
+  },
+  {
+    "revision": "554b3a40bd8b884223b05477e6b93c67",
+    "url": "/static/media/DB20160477.554b3a40.pdf"
+  },
+  {
+    "revision": "3aa35df793bfaa5ec8fb1c4a204e4b33",
+    "url": "/static/media/DBPT20160477.3aa35df7.ppt"
+  },
+  {
+    "revision": "ecc164ce0bddd97c5a96bcb2861d035e",
+    "url": "/static/media/DB20160478.ecc164ce.pdf"
+  },
+  {
+    "revision": "5a1c44530cd0e8e2fbe836d304ee5a2d",
+    "url": "/static/media/DBPT20160478.5a1c4453.ppt"
+  },
+  {
+    "revision": "33425934567bdd00432b51514f44a210",
+    "url": "/static/media/DB20170479.33425934.pdf"
+  },
+  {
+    "revision": "86b594a8b2adc556b22d934c33e29445",
+    "url": "/static/media/DBPT20170479.86b594a8.ppt"
+  },
+  {
+    "revision": "c8a57d34da77738ee5c7419ce743957e",
+    "url": "/static/media/DB20170480.c8a57d34.pdf"
+  },
+  {
+    "revision": "9361348d5fc827272d1401e5ac3d254b",
+    "url": "/static/media/DBPT20170480.9361348d.ppt"
+  },
+  {
+    "revision": "383257d868816576827beaa24b722506",
+    "url": "/static/media/img-system001.383257d8.jpg"
+  },
+  {
+    "revision": "6162dba8dbafd4d06953c5b557dfc7c1",
+    "url": "/static/media/img-system002.6162dba8.jpg"
+  },
+  {
+    "revision": "2102f6c8d746fdb1c07d8714a5ec91af",
+    "url": "/static/media/img-system003.2102f6c8.jpg"
+  },
+  {
+    "revision": "38561717d66e6f8d63eed09418610442",
+    "url": "/index.html"
+  }
+];
